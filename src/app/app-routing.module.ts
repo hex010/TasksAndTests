6803aa2ffe4b084a3c671376ce727e7eff41cb51
Robertas <robertas.nevoinas@stud.vilniustech.lt>
2023-05-03 23:17:42 +0300

    Added the necessary components, routes, bootstrap and nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TestsComponent } from './tests/tests.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:AppComponent},
+  {path: 'tests', component:TestsComponent},
+  {path: 'tasks', component:TasksComponent},
+  {path: 'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TestsComponent } from './tests/tests.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    TasksComponent,
+    TestsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
