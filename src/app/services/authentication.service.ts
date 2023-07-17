f31307004f13a9422175c7d4f5a79f32be560ee9
Robertas <robertas.nevoinas@stud.vilniustech.lt>
2023-07-17 03:22:02 +0300

    logout feature

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from "@angular/common/http";
 import { LoginRequestModel } from "../models/authenticationModels/login-request.model";
 import { RegisterRequestModel } from "../models/authenticationModels/register-request.model";
 import { AuthResponseInterface } from "../models/authenticationModels/auth-response.interface";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthenticationService {
     private registerUrl = "http://localhost:8080/api/v1/register";
     private loginUrl = "http://localhost:8080/api/v1/login";
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) {}
 
     registerUser(userData: RegisterRequestModel) {
         return this.http.post<AuthResponseInterface>(this.registerUrl, userData);
@@ -21,6 +22,11 @@ export class AuthenticationService {
 
     isLoggedIn() {
         return !!localStorage.getItem('token'); // jeigu tokenas egzistuoja, grazins true, priesingai - false.
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+        this.router.navigate(['/'])
     }
 
     getToken() {
